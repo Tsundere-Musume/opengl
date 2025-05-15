8ed6f36c348e40adeb56c4177d5ee673c6fa8297
Tsundere-Musume <maharjanelvis55@gmail.com>
2025-05-15 13:33:01 -0500

    Camera: fps camera

--- camera.h
+++ camera.h
@@ -76,6 +76,7 @@ public:
       Position -= Right * velocity;
     if (direction == RIGHT)
       Position += Right * velocity;
+	Position.y = 0.0f;
   }
 
   // processes input received from a mouse input system. Expects the offset
