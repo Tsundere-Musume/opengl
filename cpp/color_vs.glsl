923203ab8a8ec40f8fffd138e557284ec302d9e1
Tsundere-Musume <maharjanelvis55@gmail.com>
2025-05-23 08:25:52 -0500

    Exercise 3 lighting: gourard shading

--- cpp/color_vs.glsl
+++ cpp/color_vs.glsl
@@ -2,18 +2,38 @@
 layout(location = 0) in vec3 aPos;
 layout(location = 1) in vec3 aNormal;
 
-out vec3 FragPos;
-out vec3 Normal;
-out vec3 LightPos;
+out vec3 LightingColor;
 
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 uniform vec3 lightPos;
+uniform vec3 viewPos;
+uniform vec3 lightColor;
 
-void main() {
-    gl_Position = projection * view * model * vec4(aPos, 1.0);
-    FragPos = vec3(view * model * vec4(aPos, 1.0));
-    Normal = mat3(transpose(inverse(view * model))) * aNormal;
-    LightPos = vec3(view * vec4(lightPos, 1.0)); // Transform world-space light position to view-space light position
+void main()
+{
+    vec3 Normal = mat3(transpose(inverse(model))) * aNormal;
+
+    float ambientStrength = 0.2;
+    vec3 ambient = ambientStrength * lightColor;
+  	
+    // diffuse 
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(lightPos - aPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = diff * lightColor;
+    
+    // specular
+    float specularStrength = 0.5;
+    vec3 viewDir = normalize(viewPos - aPos);
+    vec3 reflectDir = reflect(-lightDir, norm);  
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+    vec3 specular = specularStrength * spec * lightColor;  
+
+    gl_Position = projection * view * vec4(aPos, 1.0);
+
+	LightingColor = ambient + diffuse + specular;
 }
+
+
